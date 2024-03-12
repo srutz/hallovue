fae8dfeb475e89fd64316ca126d99731b8ac4e80
stepan.rutz <stepan.rutz@gmx.de>
2024-03-12 12:48:06 +0100

    added hotkeys

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
-  <div class="text-2xl">Hallo Vu
+  <div class="flex flex-col gap-2 items-center">
+    <Greeter greeting="Grüße">
+      <p class="text-xs">Lorem ipsum hallo hallo</p>
+    </Greeter>
   </div>
 </template>
+<script setup lang="ts">
+import Greeter from './components/Greeter.vue'
+</script>
---
+++ src/components/Greeter.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="font-bold bg-blue-300 p-8 flex flex-col gap-8 w-64" >
+    <div class="text-xl">{{ greeting }}</div>
+    <div v-if="visible">
+        <slot ></slot>
+    </div>
+    <button class="text-sm" 
+        @click="visible=!visible">
+        {{visible ? "Hide" : "Show"}}
+    </button>
+</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useHotKeys } from '../composables/useHotKeys';
+
+const { 
+    greeting
+} = defineProps<{ greeting: string }>();
+
+const visible = ref(true)
+
+const { keys, keyboardEvent } = useHotKeys();
+
+
+</script>
